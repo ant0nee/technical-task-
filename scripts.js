6fb274d7d4ea244ffa82b9801630f7025ab17ad3
Anthony Smith <anthonysmith1057@gmail.com>
2017-09-24 13:46:53 +0100

    all php removed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,36 @@
 var highlighted = [];
 
+function addBooks(books, containerId) {
+
+	for (var book = 0; book < books.length; book++) {
+       		var volumeInfo = books[book].volumeInfo;
+       		var id = books[book].id; 
+       		var title = volumeInfo.title+" "+volumeInfo.subtitle;
+			var thumbnail = volumeInfo.imageLinks.thumbnail;
+			var authors = volumeInfo.authors.join(", ");
+			var pageCount = volumeInfo.pageCount;
+			var description = escapeHtml(volumeInfo.description);
+			var shortDesc = description.substring(0, 140);
+			if (description.length != shortDesc.length) {
+
+				shortDesc += "...";
+
+			}
+
+			document.getElementById(containerId).innerHTML += "<div class=\"book\" id=\""+id+"\" onclick=\"select(this)\" title=\""+description+"\">"+
+			"<div class=\"thumbnail\"><img class=\"thumbnail-image\" src=\""+thumbnail+"\" alt=\"book cover\" /></div>"+
+			"<div class=\"info\">"+
+			"<p class=\"title\">"+title+"</p>"+
+			"<p class=\"authors\">"+authors+"</p>"+
+			"<p class=\"pageCount\">pages: "+pageCount+"</p>"+
+			"<p class=\"description\">"+shortDesc+"</p>"+
+			"</div>"+
+			"</div>";
+
+       }
+
+}
+
 function loaded() {
 
 	toggleMenu();
@@ -19,6 +50,21 @@ function loaded() {
 		}
 
 	}
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+
+	       var books = JSON.parse(xhttp.responseText).items;
+	       var featured = books.slice(-2,books.length);
+	       var not_featured = books.slice(0, -2); 
+	       addBooks(featured, "featured");
+	       addBooks(not_featured, "not_featured");
+	       
+	    }
+	};
+	xhttp.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5", true);
+	xhttp.send();
 
 }
 
@@ -48,4 +94,13 @@ function select(object) {
 
 	localStorage.setItem("selected", JSON.stringify(highlighted));
 
+}
+
+function escapeHtml(text) {
+  return text
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
 }
